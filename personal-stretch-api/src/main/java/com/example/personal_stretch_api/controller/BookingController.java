a6390a07c3e3b81258c4c0773c1bdd61d4446503
usatai <usatai.soccer0907@gmail.com>
2025-11-17 22:56:28 +0900

    [add] 予約情報コントローラークラス作成

---
+++ personal-stretch-api/src/main/java/com/example/personal_stretch_api/controller/BookingController.java
@@ -0,0 +1,10 @@
+package com.example.personal_stretch_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1")
+@RestController
+public class BookingController {
+    
+}
