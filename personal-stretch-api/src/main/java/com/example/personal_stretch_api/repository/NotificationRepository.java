c15b00d61d2f88d6f62959a102d016d6a581c424
usatai <usatai.soccer0907@gmail.com>
2026-02-02 21:04:08 +0900

    [add] 通知機能 雛形

--- personal-stretch-api/src/main/java/com/example/personal_stretch_api/repository/NotificationRepository.java
+++ personal-stretch-api/src/main/java/com/example/personal_stretch_api/repository/NotificationRepository.java
@@ -1,9 +1,13 @@
 package com.example.personal_stretch_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.personal_stretch_api.model.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
-        
+
+    List<Notification> findByIsReadFalseOrderByCreatedAtDesc();
+
 }
