15ac63ac0980703835490ad853d582c17c14efd8
usatai <usatai.soccer0907@gmail.com>
2026-02-02 20:43:57 +0900

    [add] 通知テーブル作成

---
+++ personal-stretch-api/src/main/java/com/example/personal_stretch_api/model/Notification.java
@@ -0,0 +1,47 @@
+package com.example.personal_stretch_api.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "notifications")
+@Data
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Bookingテーブルとのリレーション
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booking_id", nullable = false)
+    private Booking booking;
+
+    @Column(name = "notification_type", nullable = false)
+    @Enumerated(EnumType.STRING) // 文字列としてDBに保存
+    private NotificationType notification_type; // NEW, CANCEL, REMINDER
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "is_read")
+    private boolean isRead = false;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public enum NotificationType {
+        NEW, CANCEL, TODAY, TOMORROW
+    }
+}
